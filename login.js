afc3c9c3c385f175fb0dce5627d49c20a8e8b47d
ishmael214 <64385652+ishmael214@users.noreply.github.com>
2020-07-02 01:19:04 -0500

    added javascript for my modals and login buttons

--- login.js
+++ login.js
@@ -1,1 +1,23 @@
 // Login / Register
+
+const loginPage = document.querySelector('header .active');
+const showLoginPage = document.querySelector('.login-container');
+const closeLoginPage = document.querySelector('#btn-close');
+
+loginPage.addEventListener('click', () => {
+    showLoginPage.style.clipPath = `circle(100% at 50% 50%)`;
+})
+
+closeLoginPage.addEventListener('click', () => {
+    showLoginPage.style.clipPath = `circle(0 at 50% 50%)`;
+})
+
+const loginToggle = document.querySelectorAll('.next-log');
+const regPage = document.querySelector('#res');
+
+
+loginToggle.forEach(item => {
+    item.addEventListener('click', () => {
+        regPage.classList.toggle('hidden');
+    })
+})
